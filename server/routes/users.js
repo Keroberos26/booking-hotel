99794572bd79f6683ff2cd031cda095e707c59d2
Keroberos <jinnguyen054@gmail.com>
2023-11-24 11:00:44 +0700

    Role-Based Authorization

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,30 +1,30 @@
 import express from 'express';
 import { createUser, deleteUser, getUser, getUsers, updateUser } from '../app/controllers/user.js';
-import { verifyToken, verifyUser } from '../utils/verifyToken.js';
+import { verifyAdmin, verifyUser } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
-router.get('/check-authentication', verifyToken, (req, res) => {
-  res.send('Authentication OK');
-});
+// router.get('/check-user/:id', verifyUser, (req, res) => {
+//   res.send('User OK');
+// });
 
-router.get('/check-user/:id', verifyUser, (req, res) => {
-  res.send('User OK');
-});
+// router.get('/check-admin/:id', verifyAdmin, (req, res) => {
+//   res.send('Admin OK');
+// });
 
 //Create
 router.post('/', createUser);
 
 //Update
-router.put('/:id', updateUser);
+router.put('/:id', verifyUser, updateUser);
 
 //Delete
-router.delete('/:id', deleteUser);
+router.delete('/:id', verifyUser, deleteUser);
 
 //GET
-router.get('/:id', getUser);
+router.get('/:id', verifyUser, getUser);
 
 //GET ALL
-router.get('/', getUsers);
+router.get('/', verifyAdmin, getUsers);
 
 export default router;
